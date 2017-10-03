5c266d0aee13bec342bb6c3dd9b39f93c9d98cc4
Hung Guei <hguei@moporgic.info>
2017-10-03 16:07:41 +0800

    setup tdl for 2048

--- agent.h
+++ agent.h
@@ -82,16 +82,45 @@ public:
 			engine.seed(int(property["seed"]));
 		if (property.find("load") != property.end())
 			load_weights(property["load"]);
+
+		patt.emplace_back(new pattern({ 0, 1, 2, 3, 4, 5 }));
+		patt.emplace_back(new pattern({ 4, 5, 6, 7, 8, 9 }));
+		patt.emplace_back(new pattern({ 0, 1, 2, 4, 5, 6 }));
+		patt.emplace_back(new pattern({ 4, 5, 6, 8, 9, 10 }));
 	}
 	~player() {
 		if (property.find("save") != property.end())
 			save_weights(property["save"]);
 	}
+	std::vector<pattern*> patt;
 
 	virtual action take_action(const board& before) {
-		action best;
+		int best = -1;
 		// TODO: select a proper action
 		// TODO: push the step into episode
+
+		float best_value = -10000;
+		board best_after = before;
+		int best_reward = -1;
+
+		for (int i = 0; i < 4; i++) {
+			int op(i);
+			board after = before;
+			int reward = after.move(op);
+			if (reward == -1) continue;
+			float value = reward;
+			for (pattern* p : patt) {
+				value += p->estimate(after);
+			}
+			if (value > best_value) {
+				best_value = value;
+				best_after = after;
+				best = op;
+				best_reward = reward;
+			}
+		}
+
+		episode.emplace_back(before, best_after, action(best), best_reward);
 		return best;
 	}
 
@@ -102,20 +131,62 @@ public:
 
 	virtual void close_episode(const std::string& flag = "") {
 		// TODO: train your agent by TD(0)
+
+		float exact = 0;
+
+		episode.pop_back();
+		while (episode.size()) {
+			auto& last = episode.back();
+
+			float current = 0;
+			for (pattern* p : patt) {
+				current += p->estimate(last.after);
+			}
+			float error = exact - current;
+			float update = error * 0.1;
+			float updated = 0;
+			for (pattern* p : patt) {
+				updated += p->update(last.after, update);
+			}
+
+			exact = updated + last.reward;
+			episode.pop_back();
+		}
 	}
 
 	virtual void load_weights(const std::string& path) {
+		std::ifstream in;
+		in.open(path.c_str(), std::ios::in | std::ios::binary);
+		if (!in.is_open()) std::exit(-1);
+		size_t size;
+		in.read(reinterpret_cast<char*>(&size), sizeof(size));
+		weights.resize(size);
+		for (weight& w : weights)
+			in >> w;
+		in.close();
 	}
 
 	virtual void save_weights(const std::string& path) {
+		std::ofstream out;
+		out.open(path.c_str(), std::ios::out | std::ios::binary | std::ios::trunc);
+		if (!out.is_open()) std::exit(-1);
+		size_t size = weights.size();
+		out.write(reinterpret_cast<char*>(&size), sizeof(size));
+		for (weight& w : weights)
+			out << w;
+		out.flush();
+		out.close();
+
 	}
 
 private:
 	std::default_random_engine engine;
 
-	std::vector<feature*> features;
+	std::vector<weight> weights;
 
 	struct state {
+		state(const board& before, const board& after, const action& move, const int& reward)
+		: before(before), after(after), move(move), reward(reward) {}
 		board before;
 		board after;
 		action move;
