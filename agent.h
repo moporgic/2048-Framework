670d80674c92ef4aa7f7807b06d92220ed953889
Hung Guei <hguei@moporgic.info>
2017-12-05 15:18:59 +0800

    fix const issue

--- agent.h
+++ agent.h
@@ -26,9 +26,9 @@ public:
 	virtual bool check_for_win(const board& b) { return false; }
 
 public:
-	virtual std::string name() const { return property["name"]; }
-	virtual std::string role() const { return property["role"]; }
-	virtual void notify(const std::string& msg) { property[msg.substr(0, msg.find('='))] = msg.substr(msg.find('=') + 1); }
+	virtual std::string name() const { return property.at("name"); }
+	virtual std::string role() const { return property.at("role"); }
+	virtual void notify(const std::string& msg) { property[msg.substr(0, msg.find('='))] = { msg.substr(msg.find('=') + 1) }; }
 
 protected:
 	typedef std::string key;
